8a5d5319677e8fb4f26b8cd30fe5e8afa13c7470
vlad <vladislavzorin-2003@mail.ru>
2023-05-03 21:58:43 +0300

    finish

---
+++ OneDrive/Рабочий стол/DemoEx/DemoEx/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DemoEx
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isAuthError = false;
+        private int ticksLeft = 10;
+        private DispatcherTimer _timer;
+        private List<User> users; 
+        // функция входа 
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            Trade123Entities db = new Trade123Entities();
+            users = db.User.ToList();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var user = users.Find(u => u.UserLogin == TextBox_Login.Text && u.UserPassword == TextBox_Password.Text);
+
+            if (isAuthError)
+            {
+                if (Text_Captcha.Text != TextBox_Captcha.Text || user == null)
+                {
+                    MessageBox.Show("Вы не прошли проверку на работа или ввели неверный логин или пароль. Подождите 10 секунд и попробуйте снова");
+                    Button_Autorization.IsEnabled = false;
+                    Button_Guest.IsEnabled = false;
+                    var timer = new DispatcherTimer();
+                    timer.Tick += timer_Tick;
+                    timer.Interval = TimeSpan.FromSeconds(1);
+                    timer.Start();
+
+                    _timer = timer;
+
+                    Text_Captcha.Text = generateCaptcha(4);
+
+                    return;
+                }
+            }            
+
+            if (user != null)
+            {
+                MessageBox.Show("Авторизация прошла успешно!");
+                MainWindow1 wind = new MainWindow1(user);
+                wind.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели неверный логин или пароль");
+                Stack_Captcha.Visibility = Visibility.Visible;
+                Text_Captcha.Text = generateCaptcha(4);
+
+                isAuthError = true;
+            }            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ticksLeft--;
+            if (ticksLeft == 0)
+            {
+                _timer.Stop();
+                Button_Autorization.IsEnabled = true;
+                Button_Guest.IsEnabled = true;
+                ticksLeft = 10;
+            }
+        }
+
+        private string generateCaptcha(int count)
+        {
+            string captcha = "";
+            Random rand = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                switch (rand.Next(1, 3))
+                {
+                    case 1:
+                        captcha += Convert.ToChar(rand.Next(65, 90));
+                        break;
+                    case 2:
+                        captcha += Convert.ToChar(rand.Next(48, 57));
+                        break;
+                }                
+            }
+
+            return captcha;
+        }
+
+        private void TextBox_TextChanged_1(object sender, object e)
+        {
+
+        }
+
+        private void Button_Click(object sender, object e)
+        {
+           
+        }
+
+        private void Button_Guest_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+            user.UserName = "Гость";
+            MainWindow1 wind = new MainWindow1(user);
+            wind.Show();
+            this.Close();   
+        }
+    }
+}
---
+++ OneDrive/Рабочий стол/DemoEx/DemoEx/MainWindow1.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DemoEx
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow1.xaml
+    /// </summary>
+    public partial class MainWindow1 : Window
+    {
+        private List<Product> _products;
+        public MainWindow1(User user)
+        {
+            InitializeComponent();
+            Trade123Entities db = new Trade123Entities();
+            _products = db.Product.ToList();
+            lvProducts.ItemsSource = _products;
+            textAll.Text = _products.Count.ToString();
+
+            ComboBoxFilterProductDiscountAmount.ItemsSource = new List<string>
+            {
+                "Все", "0-10%", "10-15%", "15-∞%"
+            };
+            ComboBoxFilterProductByPrice.ItemsSource = new List<string>
+            {
+                "По умолчанию", "По возрастанию", "По убыванию"
+            };
+
+            ComboBoxFilterProductDiscountAmount.SelectedIndex = 0;
+            ComboBoxFilterProductByPrice.SelectedIndex = 0;
+        }
+
+        private void ComboBoxFilterProductDiscountAmount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sortProducts();
+            refreshCurrentCountProducts();
+        }
+
+        private void ComboBoxFilterProductByPrice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sortProducts();
+            refreshCurrentCountProducts();
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            sortProducts();
+            refreshCurrentCountProducts();
+        }
+
+        private void refreshCurrentCountProducts()
+        {
+            textCurrent.Text = lvProducts.ItemsSource.Cast<Product>().Count().ToString();
+        }
+
+        private void sortProducts()
+        {
+            var products = _products;
+
+            switch (ComboBoxFilterProductDiscountAmount.SelectedIndex)
+            {
+                case 0:
+                    {
+                        break;
+                    }
+                case 1:
+                    {
+                        products = products.Where(p => p.ProductDiscountAmount < 10).ToList();
+                        break;
+                    }
+                case 2:
+                    {
+                        products = products.Where(p => p.ProductDiscountAmount > 10 && p.ProductDiscountAmount < 15).ToList();
+                        break;
+                    }
+                case 3:
+                    {
+                        products = products.Where(p => p.ProductDiscountAmount > 15).ToList();
+                        break;
+                    }
+            }
+
+            switch (ComboBoxFilterProductByPrice.SelectedIndex)
+            {
+                case 0:
+                    {
+                        break;
+                    }
+                case 1:
+                    {
+                        products = products.OrderBy(p => p.ProductCost).ToList();
+                        break;
+                    }
+                case 2:
+                    {
+                        products = products.OrderByDescending(p => p.ProductCost).ToList();
+                        break;
+                    }
+            }
+
+            if (tbSearch.Text != "" && !(string.IsNullOrWhiteSpace(tbSearch.Text)))
+            {
+                products = products.FindAll(p => p.ProductName.Contains(tbSearch.Text));
+            }
+
+            lvProducts.ItemsSource = products;
+        }        
+    }
+}
